53e72bfbd91c099b95aa700e1ff748e13705f8a5
lexicon06 <pablo.hehe2@gmail.com>
2024-02-06 16:11:25 -0300

    updated credenciales.js to import the config data

--- react-firebase-practice/src/firebase/credenciales.js
+++ react-firebase-practice/src/firebase/credenciales.js
@@ -2,16 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth'; //servicio de autenticación
 import { getFirestore } from 'firebase/firestore'; //servicio base de datos
 import { getStorage } from 'firebase/storage'; //servericio almacenamiento
-
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCT_kIvhVuv3NSvFq4ooiQk7NUDFU1ZpLw",
-  authDomain: "codigo-facilito-react-firebase.firebaseapp.com",
-  projectId: "codigo-facilito-react-firebase",
-  storageBucket: "codigo-facilito-react-firebase.appspot.com",
-  messagingSenderId: "800352007082",
-  appId: "1:800352007082:web:7961063cd25afa668171d3"
-};
+import firebaseConfig from './config';
 
 
 const firebaseApp = initializeApp(firebaseConfig);//initialize the app
