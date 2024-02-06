9d04244d8fac517b38edca26efdf1f7cdd5e7f4f
lexicon06 <pablo.hehe2@gmail.com>
2024-02-05 23:58:19 -0300

    implmeneted login with Google Auth

---
+++ react-firebase-practice/src/functions/loginWithGoogle.js
@@ -0,0 +1,21 @@
+import { auth } from '../firebase/credenciales';
+import { GoogleAuthProvider, 
+    signInWithPopup, 
+    signInWithRedirect } from 'firebase/auth';
+
+
+export default async function loginWithGoggle(){
+    try{
+
+        const provider = new GoogleAuthProvider();
+        signInWithRedirect(auth, provider);
+
+    }catch(error){
+
+    }
+
+}
+
+
+
+
