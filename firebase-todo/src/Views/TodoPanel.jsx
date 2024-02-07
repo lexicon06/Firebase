b0e2328da2d18a566a8e250c35a14ed65d828c0f
lexicon06 <pablo.hehe2@gmail.com>
2024-02-07 19:16:56 -0300

    Display items successfully based on useremail to catch the ones that belongs to the current user

--- firebase-todo/src/Views/TodoPanel.jsx
+++ firebase-todo/src/Views/TodoPanel.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 import Firestore from "../functions/databaseActions";
 
 function TodoPanel({userFirebase}) {
+
+    const [todos, setTodos] = React.useState([]);
+    const [update, setUpdate] = React.useState(false);
+
+    React.useEffect(() => {
+        async function fetchData() {
+            const data = await Firestore.Read(userFirebase.email);
+            setTodos(data);
+        }
+        fetchData();
+    }, [update]);
+
+
     async function formHandler(e){
         e.preventDefault();
         const data = e.target.elements.todo.value;
@@ -9,11 +22,23 @@ function TodoPanel({userFirebase}) {
         e.target.reset();
     }
   return <>
+    <div className="addContainer">
     <div>TodoPanel</div>
     <form onSubmit={formHandler}>
         <label htmlFor="todo"></label><input type="text" id="todo"/>
-        <button type="submit">Add</button>
+        <button type="submit" onClick={()=>{
+            setUpdate(!update);
+        }}>Add</button>
     </form>
+    </div>
+    <div className="dbContainer">
+    {todos.map((todo, index) => (
+        <div key={index}>
+            User: {todo.user}, Item: {todo.item}
+        </div>
+    ))}
+</div>
+
   </>
 }
 
--- firebase-todo/src/functions/databaseActions.js
+++ firebase-todo/src/functions/databaseActions.js
@@ -1,50 +1,59 @@
 import { db } from "../firebase/credentials";
-import { collection, addDoc, getDocs, doc, updateDoc, deleteDoc } from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  doc,
+  updateDoc,
+  deleteDoc,
+  query,
+  where,
+} from "firebase/firestore";
 
-
-async function addTodoElement(data){
-    try {
-        const collectionRef = collection(db, "todo");
-        const todoId = await addDoc(collectionRef, data);
-        return todoId;
-    } catch (error) {
-        console.error("Error adding document: ", error);
-    }
+async function addTodoElement(data) {
+  try {
+    const collectionRef = collection(db, "todo");
+    const todoId = await addDoc(collectionRef, data);
+    return todoId;
+  } catch (error) {
+    console.error("Error adding document: ", error);
+  }
 }
 
-async function getTodoElements(){
-    try {
-        const collectionRef = collection(db, "todo");
-        const snapshot = await getDocs(collectionRef);
-        return snapshot.docs.map(doc => doc.data());
-    } catch (error) {
-        console.error("Error getting documents: ", error);
+async function getTodoElements(id = "nothing") {
+  try {
+    let collectionRef = collection(db, "todo");
+    if (id !== "nothing") {
+      collectionRef = query(collectionRef, where("user", "==", id));
     }
+    const snapshot = await getDocs(collectionRef);
+    return snapshot.docs.map((doc) => doc.data());
+  } catch (error) {
+    console.error("Error getting documents: ", error);
+  }
 }
 
-async function updateTodoElement(id, data){
-    try {
-        const docRef = doc(db, "todo", id);
-        await updateDoc(docRef, data);
-    } catch (error) {
-        console.error("Error updating document: ", error);
-    }
+async function updateTodoElement(id, data) {
+  try {
+    const docRef = doc(db, "todo", id);
+    await updateDoc(docRef, data);
+  } catch (error) {
+    console.error("Error updating document: ", error);
+  }
 }
 
-
-async function deleteTodoElement(id){
-    try {
-        const docRef = doc(db, "todo", id);
-        await deleteDoc(docRef);
-    } catch (error) {
-        console.error("Error deleting document: ", error);
-    }
+async function deleteTodoElement(id) {
+  try {
+    const docRef = doc(db, "todo", id);
+    await deleteDoc(docRef);
+  } catch (error) {
+    console.error("Error deleting document: ", error);
+  }
 }
 
-
-export default { 
-    Create: addTodoElement,
-    Read: getTodoElements,
-    Update: updateTodoElement,
-    Delete: deleteTodoElement
+export default {
+  Create: addTodoElement,
+  Read: getTodoElements,
+  Update: updateTodoElement,
+  Delete: deleteTodoElement,
 };
